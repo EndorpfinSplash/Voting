377ac8c4e99be6db2d913b6a90b478854fe62645
AZ <fast.optimus@gmail.com>
2019-05-07 20:25:42 +0300

    Swagger dependency add

--- src/main/java/com/itacademy/repository/RoleDao.java
+++ src/main/java/com/itacademy/repository/RoleDao.java
@@ -5,7 +5,7 @@ import com.itacademy.domain.Role;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public interface RoleDao extends GenericDao<com.itacademy.domain.Role, Long> {
+public interface RoleDao extends GenericDao<Role, Long> {
 
     Role getRoleFromRow(ResultSet resultSet, int i)  throws SQLException;
 
