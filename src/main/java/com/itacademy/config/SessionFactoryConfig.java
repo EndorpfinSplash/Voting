4a967c6926834845ad44739563b63a424e340602
AZ <fast.optimus@gmail.com>
2019-05-09 11:35:58 +0300

    SessionFactory Config add SpringBootHiberAppl add

---
+++ src/main/java/com/itacademy/config/SessionFactoryConfig.java
@@ -0,0 +1,36 @@
+package com.itacademy.config;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+public class SessionFactoryConfig {
+    @Autowired
+    @Bean(name = "sessionFactory")
+    public SessionFactory getSessionFactory(DataSource dataSource) throws Exception {
+        Properties properties = new Properties();
+
+        // See: application.properties
+        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+        properties.put("hibernate.show_sql", "true");
+        properties.put("current_session_context_class", "org.springframework.orm.hibernate5.SpringSessionContext");
+
+        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+
+        // Package contain entity classes
+        factoryBean.setPackagesToScan("com.htp");
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setHibernateProperties(properties);
+        factoryBean.afterPropertiesSet();
+        //
+        SessionFactory sf = factoryBean.getObject();
+        System.out.println("## getSessionFactory: " + sf);
+        return sf;
+    }
+
+}
+
