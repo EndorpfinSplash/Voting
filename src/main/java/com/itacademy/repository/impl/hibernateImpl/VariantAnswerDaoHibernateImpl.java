81ae44efeb80353dfd8c10847777e3a978ab1d66
AZ <fast.optimus@gmail.com>
2019-05-09 16:50:38 +0300

    Add Hiberante impl and controllers.

---
+++ src/main/java/com/itacademy/repository/impl/hibernateImpl/VariantAnswerDaoHibernateImpl.java
@@ -0,0 +1,92 @@
+package com.itacademy.repository.impl.hibernateImpl;
+
+import com.itacademy.domain.Poll;
+import com.itacademy.domain.VariantAnswer;
+import com.itacademy.repository.VariantAnswerDao;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Qualifier("variantAnswerDaoHibernateImpl")
+public class VariantAnswerDaoHibernateImpl implements VariantAnswerDao {
+
+    @Autowired
+    @Qualifier("sessionFactory")
+    private SessionFactory sessionFactory;
+
+    @Override
+    public List<VariantAnswer> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("SELECT v FROM VariantAnswer v", VariantAnswer.class).list();
+        }
+    }
+
+    @Override
+    public VariantAnswer findById(Long id) {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("SELECT v FROM VariantAnswer v where v.pollId = :id", VariantAnswer.class).uniqueResult();
+        }
+    }
+
+    @Override
+    public void delete(Long id) {
+        VariantAnswer variantAnswer = findById(id);
+        try (Session session = sessionFactory.openSession()) {
+            session.delete(variantAnswer);
+        }
+    }
+
+    @Override
+    public VariantAnswer save(VariantAnswer entity) {
+
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.save(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        return entity;
+    }
+
+    @Override
+    public VariantAnswer update(VariantAnswer entity) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.saveOrUpdate(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback(); }
+            e.printStackTrace();
+        }
+        return entity;
+    }
+
+    @Override
+    public List<VariantAnswer> findVariantAnswersForPull(Poll poll) {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("SELECT v FROM VariantAnswer v where v.pollId = :id", VariantAnswer.class)
+                    .setParameter("id", poll.getPollId())
+                    .list();
+        }
+    }
+
+    @Override
+    public List<VariantAnswer> findVariantAnswersForPull(Long poll_id) {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("SELECT v FROM VariantAnswer v where v.pollId = :id", VariantAnswer.class).list();
+        }
+    }
+}
